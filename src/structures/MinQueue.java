80337127058b1ab0426c930e50875efb88b12d0d
Lucas Chagas <lucas.w.chagas@gmail.com>
2022-06-11 14:34:33 -0400

    Initial commit

---
+++ src/structures/MinQueue.java
@@ -0,0 +1,69 @@
+package structures;
+
+import comparators.IntegerComparator;
+import comparators.ReverseIntegerComparator;
+
+import java.util.Comparator;
+import java.util.Iterator;
+
+public class MinQueue<V> implements PriorityQueue<Integer, V> {
+
+	private StudentArrayHeap<Integer, V> queue;
+	private Comparator<Integer> comparator;
+	
+	public MinQueue() {
+		comparator = new ReverseIntegerComparator();
+		queue = new StudentArrayHeap<Integer, V>(comparator);
+	}
+	
+	@Override
+	public PriorityQueue<Integer, V> enqueue(Integer priority, V value) throws NullPointerException {
+		if(priority == null || value == null) {
+			throw new NullPointerException();
+		}
+		queue.add(priority, value);
+		return this;
+	}
+
+	@Override
+	public V dequeue() throws IllegalStateException {
+		if(isEmpty()) {
+			throw new IllegalStateException();
+		}
+		V elem = queue.remove();
+		return elem;
+	}
+
+	@Override
+	public V peek() throws IllegalStateException {
+		if(isEmpty()) {
+			throw new IllegalStateException();
+		}
+		V elem = queue.peek();
+		return elem;
+	}
+
+	@Override
+	public Iterator<Entry<Integer, V>> iterator() {
+		return queue.asList().iterator();
+	}
+	
+	@Override
+	public Comparator<Integer> getComparator() {
+		return comparator;
+	}
+
+	@Override
+	public int size() {
+		return queue.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		if(size() == 0) {
+			return true;
+		}
+		return false;
+	}
+}
+
